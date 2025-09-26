71b8b550e5e61567573e983af78f29ac739d6276
Axel Gschaider <axel.gschaider@posteo.at>
2025-09-27 00:41:33 +0200

    add args parser

--- ze-ueber-generator/app/Main.hs
+++ ze-ueber-generator/app/Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
+import Options.Applicative
+import Control.Monad       (join)
+
+data Args = Args {
+    args'prefix  :: FilePath
+  , args'data    :: FilePath
+  , args'postfix :: FilePath
+  , args'target  :: FilePath
+} deriving (Show)
+
+argsParser :: Parser Args
+argsParser = Args <$> fileParam "prefix" "file that holds the prefix of the targetfile before the table"
+                  <*> fileParam "data" "yaml file holding the primary data"
+                  <*> fileParam "postfix" "file that holde the postfix of the targetfile after the table"
+                  <*> fileParam "target" "target file to generate"
+  where
+    fileParam l h = strOption (long l <> metavar "FILE" <> help h)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- execParser $ info argsParser fullDesc
+    putStrLn "Hello, Haskell!"
+    putStrLn $ show args
