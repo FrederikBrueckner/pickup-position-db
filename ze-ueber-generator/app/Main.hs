3b8b931bd74bd6155376dd7dc9df28e71adcf30a
Axel Gschaider <axel.gschaider@posteo.at>
2025-09-27 03:16:19 +0200

    Write YAML Parser

--- ze-ueber-generator/app/Main.hs
+++ ze-ueber-generator/app/Main.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Options.Applicative
-import Control.Monad       (join)
-import System.IO           (openFile, IOMode(ReadMode), hGetContents, hClose)
+import           Data.Text              (Text)
+import           Data.Yaml              (FromJSON(..), withObject, decodeFileThrow, (.:), (.:?))
+
+import           Options.Applicative    (execParser, strOption, long, metavar, help, info, fullDesc)
+import qualified Options.Applicative as OA
+
 
 data Args = Args {
     args'prefix  :: FilePath
@@ -11,7 +16,7 @@ data Args = Args {
   , args'target  :: FilePath
 } deriving (Show)
 
-argsParser :: Parser Args
+argsParser :: OA.Parser Args
 argsParser = Args <$> fileParam "prefix" "file that holds the prefix of the targetfile before the table"
                   <*> fileParam "data" "yaml file holding the primary data"
                   <*> fileParam "postfix" "file that holde the postfix of the targetfile after the table"
@@ -21,9 +26,57 @@ argsParser = Args <$> fileParam "prefix" "file that holds the prefix of the targ
 
 main :: IO ()
 main = do
-    args <- execParser $ info argsParser fullDesc
-    x    <- readFile $ args'prefix args
+    args    <- execParser $ info argsParser fullDesc
+    rawData <- decodeFileThrow $ args'data args
+    let lineData = toLineData rawData
 
     putStrLn "LOL"
-    putStrLn x
+    putStrLn $ show rawData
 
+toLineData :: [Yaml'Instrument] -> [LineData]
+toLineData = const []
+
+data LineData = LineData {
+      ld'instrumentID :: Int
+    , ld'brand        :: Text
+    , lb'make         :: Text
+    , lb'scale        :: Float
+    , lb'description  :: Text
+    , lb'value        :: Float
+    , lb'reporter     :: Maybe Text
+    , lb'comment      :: Maybe Text
+    } deriving (Show)
+
+-- representation of parsed data
+
+data Yaml'Measurements = Yaml'Measurements {
+      ym'description :: Text
+    , ym'value       :: Float
+    , ym'scale       :: Maybe Float
+    , ym'comment     :: Maybe Text
+    } deriving (Show)
+
+instance FromJSON Yaml'Measurements where
+  parseJSON = withObject "Measurement" $ \o ->
+                Yaml'Measurements <$> o .:  "description"
+                                  <*> o .:  "value"
+                                  <*> o .:? "scale"
+                                  <*> o .:? "comment"
+
+data Yaml'Instrument = Yaml'Instrument {
+      yi'brand        :: Text
+    , yi'make         :: Text
+    , yi'scale        :: Maybe Float
+    , yi'reporter     :: Maybe Text
+    , yi'comment      :: Maybe Text
+    , yi'measurements :: [Yaml'Measurements]
+    } deriving (Show)
+
+instance FromJSON Yaml'Instrument where
+  parseJSON = withObject "Instrument" $ \o ->
+                Yaml'Instrument <$> o .:  "brand"
+                                <*> o .:  "make"
+                                <*> o .:? "scale"
+                                <*> o .:? "reporter"
+                                <*> o .:? "comment"
+                                <*> o .:  "measurements"
